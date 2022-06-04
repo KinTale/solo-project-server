13a0365a12a3d352f06c570a96342cffecd60ad8
Kiran Gurung <kg820532@gmail.com>
2022-06-04 17:44:35 +0100

    update controller

--- src/controller/inventory.js
+++ src/controller/inventory.js
@@ -1,11 +1,9 @@
 import dbClient from '../utils/dbClient.js'
 
-
 export const inventoryList = async (req, res) => {
-
     try {
         const list = await dbClient.inventory.findMany()
-        return res.status(200).json({ data: list})
+        return res.status(200).json({ data: list })
     } catch (e) {
         return res.status(500).json('unable to get members')
     }
@@ -14,7 +12,7 @@ export const inventoryList = async (req, res) => {
 export const addItem = async (req, res) => {
     const { description, quantity, sponsored, price, location } = req.body
     const adminId = req.adminId
-    console.log( description, quantity, sponsored, price, location)
+    console.log(description, quantity, sponsored, price, location)
     try {
         const createdItem = await dbClient.inventory.create({
             data: {
@@ -30,33 +28,51 @@ export const addItem = async (req, res) => {
                 }
             }
         })
-        
         return res.status(201).json(createdItem)
     } catch (e) {
-// res.status(500).json('unable to create item')
+        // res.status(500).json('unable to create item')
         return console.log(e)
     }
 }
 
 export const editItem = async (req, res) => {
-    
+    const { id, description, quantity, sponsored, price, location } = req.body
+    const adminId = req.adminId
+    try {
+        const updateItem = await dbClient.inventory.update({
+            where: {
+                id: id
+            },
+            data: {
+                description: description,
+                quantity: parseInt(quantity),
+                sponsored: sponsored,
+                price: parseInt(price),
+                location: location,
+                admin: {
+                    connect: {
+                        id: adminId
+                    }
+                }
+            }
+        })
+        console.log(updateItem)
+        return res.status(201).json(updateItem)
+    } catch (e) {
+        return console.log(e)
+    }
 }
-
 export const deleteItem = async (req, res) => {
-    console.log(req.params.itemId)
     const itemId = parseInt(req.params.itemId)
-     
-    try{
+    try {
         const deletedItem = await dbClient.inventory.delete({
-            where:{
+            where: {
                 id: itemId
             }
         })
         return res.status(201).json(deletedItem)
-    }catch (e) {
+    } catch (e) {
         return res.status(500).json('unable to delete item')
     }
-
-
 
 }
